122d145baccd2804ce31a7f4acdd8463cbff3d9f
Mikaeilkhaste <124209169+Mikaeilkhaste@users.noreply.github.com>
2024-05-13 22:35:24 +0330

    Update customipblocker.sh

--- customipblocker.sh
+++ customipblocker.sh
@@ -2,9 +2,14 @@
 
 # Function to display the menu
 display_menu() {
+    echo "**********"
+    echo "***  Lazy Script  ****"
+    echo "**********"
     echo "Choose an option:"
     echo "1. Block custom IP range"
-    echo "2. Exit"
+    echo "2. Unblock previously blocked IP ranges"
+    echo "3. Exit"
+    echo "**********"
 }
 
 # Function to block IP range
@@ -21,6 +26,17 @@ block_ip_range() {
     echo "IP range blocked successfully."
 }
 
+# Function to unblock previously blocked IP ranges
+unblock_ip_ranges() {
+    # Flush all iptables rules
+    iptables -F
+
+    # Save the iptables rules to make them persistent
+    iptables-save > /etc/iptables/rules.v4
+
+    echo "All previously blocked IP ranges have been unblocked."
+}
+
 # Main loop
 while true; do
     display_menu
@@ -30,6 +46,9 @@ while true; do
             block_ip_range
             ;;
         2)
+            unblock_ip_ranges
+            ;;
+        3)
             echo "Exiting..."
             exit 0
             ;;
